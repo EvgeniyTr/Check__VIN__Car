d066c0aa4f478d52ee7f11b77b0e988c389ba63b
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-07-05 17:51:13 +0400

    main page

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import {ChakraProvider} from "@chakra-ui/react"
+import AppContainer from "../components/AppContainer"
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <AppContainer>
+        <Component {...pageProps}/>
+      </AppContainer>
+    </ChakraProvider>
+  )
+}
+
+export default  MyApp;
