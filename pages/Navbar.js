7fad62f622ab76846bf0b82c1b95ba5c2386299a
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-07-05 17:51:47 +0400

    created navbar

---
+++ pages/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Box,Link,Text} from "@chakra-ui/react"
+
+const Navbar = () => {
+  return (
+   <Box display="flex" w={"full"} h="100px" bg={"blue.400"}>
+    <Box display="flex" justifyContent={"space-between"} alignItems="center" w={"100%"} >
+        <Box display="flex" >
+        <Link mt="40px" ml={"52px"} fontSize={"2xl"} href="/" >CheckCar</Link>
+        </Box>
+        <Box display={"flex"} justifyContent="flex-end">
+        <Text mt="40px" mr="58px"  fontSize={"2xl"}>EN</Text> 
+        </Box>
+    
+       
+   
+    </Box>
+   
+   </Box>
+  )
+}
+
+export default Navbar
