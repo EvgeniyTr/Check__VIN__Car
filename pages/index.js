363ed85a30027196bb3df1165a47297f0d311e76
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-07-05 17:51:34 +0400

    index

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import {Container,FormControl,FormLabel,Input,Heading,Textarea,FormErrorMessage,Button,Text, useToast} from "@chakra-ui/react"
+import Navbar from "./Navbar"
+import Form from "./Form"
+
+
+
+export default function Home() {
+  return (
+    <Container >
+    <Form />
+    </Container>
+  )
+}
