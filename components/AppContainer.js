e9c97a1de9f0a08dfacb0fa03c9fccb8250189a7
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-07-19 00:26:51 +0400

    push

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -1,10 +1,19 @@
-import Navbar from "../pages/Navbar";
-
+import Navbar from "./Navbar";
+import Form from "./Form";
+import Hero from "./Hero";
+import Reasons from "./Reasons";
+import SampleButton from "./SampleButton";
+import Footer from "./Footer";
 
 const AppContainer = ({children}) => {
     return (
         <>
         <Navbar />
+        <Form />
+    <Hero />
+    <Reasons />
+    <SampleButton />
+    <Footer />
             {children}
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
-import Form from "./Form"
-import Hero from "./Hero"
-import Reasons from "./Reasons"
-import SampleButton from "./SampleButton"
-import Footer from "./Footer"
 
 
 export default function Home() {
   return (
     <>
-    <Form />
-    <Hero />
-    <Reasons />
-    <SampleButton />
-    <Footer />
     </>
   )
 }
