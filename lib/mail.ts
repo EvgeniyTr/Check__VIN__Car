5b0b7867eb7c9aca72098aa641e3ae0cca5e0320
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-28 21:38:55 +0400

    add pdf attackment to mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -13,7 +13,15 @@ export const sendMail = async (
     from: 'mts.sturua@gmail.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    attackments: [
+      {
+        content: pdfBuffer.toString('base64'),
+        filename: 'report.pdf',
+        type: 'application/pdf',
+        disposition: 'attachment'
+      }
+    ]
   }
 
   try {
