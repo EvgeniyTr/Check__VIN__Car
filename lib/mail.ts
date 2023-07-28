5c0fc7b13ad29dee19e49cc9a3fa55b78de85ff3
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2023-07-28 17:52:23 +0400

    throw error in sendmail and lint code

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,18 +3,23 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(getEnvVar('SENDGRID_API_KEY'))
 
-export const sendMail = async (mail: string, pdfBuffer: Buffer, tries: number = 3) => {
+export const sendMail = async (
+  mail: string,
+  pdfBuffer: Buffer,
+  tries: number = 3
+) => {
   const msg = {
     to: mail, // Change to your recipient
     from: 'mts.sturua@gmail.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
   }
 
   try {
     await sgMail.send(msg)
   } catch (err) {
     if (tries > 0) return sendMail(mail, pdfBuffer, tries - 1)
+    throw new Error('Could not send mail')
   }
 }
