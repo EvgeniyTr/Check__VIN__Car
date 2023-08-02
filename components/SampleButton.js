64775ff54a8326443e3a4469e6835251918e12cf
Kaxa Imerlishvili <70231512+real0ta@users.noreply.github.com>
2023-08-02 11:50:03 +0400

    add sample report and serve it (#34)
    Co-authored-by: kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>

--- components/SampleButton.js
+++ components/SampleButton.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Button, Box } from '@chakra-ui/react'
+import { Button, Box, Link } from '@chakra-ui/react'
 
 const SampleButton = () => {
   return (
     <Box display="flex" justifyContent="center" mt="3%">
-      <Button
+      <Link
+        href="/Sample.pdf"
+        target="_blank"
         bg="blue.500"
         color="white"
         w={[300, 400, 450]}
-        h="50px"
+        h="60px"
         borderRadius="2xl"
+        textAlign="center"
+        p=".8rem"
+        fontWeight="bold"
         fontSize={{ base: '18px', md: '20px', lg: '25px' }}
       >
         VIEW SAMPLE REPORT
-      </Button>
+      </Link>
     </Box>
   )
 }
